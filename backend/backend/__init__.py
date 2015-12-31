c0f4c08d53e92f067bab38121373e8ff24778ab9
Tomohiro Nakamura <quickness.net@gmail.com>
2015-12-31 10:36:36 +0900

    update backend

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -5,7 +5,7 @@ from pyramid.config import Configurator
 if __name__ == '__main__':
     config = Configurator()
     config.include('cornice')
-    config.scan('views')
+    config.scan('resource')
     app = config.make_wsgi_app()
 
     (make_server('0.0.0.0', 8080, app)
