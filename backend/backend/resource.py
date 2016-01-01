5aee605a97fa6a68f651597ee307b99e4c8b5c54
Tomohiro Nakamura <quickness.net@gmail.com>
2016-01-01 18:49:58 +0900

    backend: add comment

--- backend/backend/resource.py
+++ backend/backend/resource.py
@@ -3,6 +3,8 @@ from pyramid.exceptions import Forbidden
 from cornice import Service
 
 
+# easy to develop
+# use dictionary on memory
 PRODUCTS = {
     '1': {
         'id': '1',
@@ -19,32 +21,44 @@ PRODUCTS = {
 }
 
 
+# api declaration
 cors_policy = {'enabled': True, 'origins': ('*',)}
 prod_source = Service(
-    name='data_source',
+    name='products api',
     path='/products',
-    description='some',
+    description='get or update product data',
     cors_policy=cors_policy)
 
 
 @prod_source.get()
 def get_products(request):
+    '''Get products data.
+
+    If the function spcified product id, then return such product data.
+    '''
     if 'id' in request.GET:
         return [PRODUCTS.get(str(request.GET['id']))]
     else:
-        return list(PRODUCTS.values())
+        return sorted(list(PRODUCTS.values()), key=lambda x: x['id'])
 
 
 @prod_source.put()
-def set_products(request):
+def upsert_products(request):
+    '''Upsert product data.'''
     data = request.json
-    if all(x in data for x in ['id', 'name', 'price']):
-        item_id = str(data['id'])
-        PRODUCTS[item_id] = {'id': item_id, 'name': data['name'], 'price': int(data['price'])}
-        return {'success': True}
-    elif all(x in data for x in ['name', 'price']):
-        item_id = str(PRODUCTS.keys() + 1)
-        PRODUCTS[item_id] = {'id': item_id, 'name': data['name'], 'price': int(data['price'])}
+    if 'name' in data and 'price' in data:
+        # compute id
+        item_id = data.get('id')
+        if not item_id:
+            _item_ids = [int(x) for x in PRODUCTS.keys() if x.isdigit()]
+            item_id = str(max(_item_ids) + 1)
+
+        # upsert
+        PRODUCTS[item_id] = {
+            'id': item_id,
+            'name': data['name'],
+            'price': int(data['price'])}
+
         return {'success': True}
     else:
         raise Forbidden()
